497b29e6f43156b79732a4b82765fd61f86a98f2
Camilo Delgado <camilodelgado0305@gmail.com>
2024-08-17 10:30:55 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["https://lanet.app.la-net.co", "http://localhost:5173"],
+    origin: ["http://lanet.app.la-net.co", "http://localhost:5173"],
   })
 );
 app.use(express.json());
